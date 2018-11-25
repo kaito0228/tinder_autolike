6771de5c02f8d3a6d172ce08f573a2443fed6e60
ryo.s <aqua.timez.saiko@gmail.com>
2018-11-26 00:24:16 +0900

    Randomize the time between flicks

--- tinder.py
+++ tinder.py
@@ -1,23 +1,24 @@
+import random
 import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-i=1
+i = 1
 chrome = webdriver.Chrome()
 
 # Tinderに飛ぶ
 chrome.get("https://tinder.com/app/login")
 
 # 手動でログインしたら、yキーを1回押してスタート
-# 0.3秒ごとに、右キー（いいね）を入力、3000人まで行ったら、終了
 key = input('Tinderのトップページが出たらy→Enterを押してください')
 if key == "y":
     # ↓の < の右にある数字でフリック数の上限を指定
-    while i <= 3000:
-        i=i+1
+    while i <= 2000:
+        i = i + 1
         chrome.find_element_by_tag_name("body").send_keys(Keys.RIGHT)
-        # ↓の（）内の数字でフリック間の秒数を指定
-        time.sleep(0.3)
+        # (a,b)の間の秒数ランダムでフリック
+        time.sleep(random.uniform(0.5, 3))
         print(i)
     else:
         print("3000人にいいねしました")
